b2827b27c7b0c0ef17dd2b65bc4c2e35b0453c1d
XIERRA <52464725+Xierra-Dev@users.noreply.github.com>
2024-12-19 09:07:12 +0700

    ++

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\dev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\naufa\\Downloads\\pbo_meal_planner" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "PROJECT_DIR=C:\\Users\\naufa\\Downloads\\pbo_meal_planner"
+  "FLUTTER_ROOT=C:\\dev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\naufa\\Downloads\\pbo_meal_planner\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\naufa\\Downloads\\pbo_meal_planner"
+  "FLUTTER_TARGET=C:\\Users\\naufa\\Downloads\\pbo_meal_planner\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\naufa\\Downloads\\pbo_meal_planner\\.dart_tool\\package_config.json"
+)
