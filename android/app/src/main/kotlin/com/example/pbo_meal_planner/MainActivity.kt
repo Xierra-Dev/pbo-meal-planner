4ff34eff89e7692a0eaa8d24a03b709217fc1201
XIERRA <52464725+Xierra-Dev@users.noreply.github.com>
2024-12-25 21:04:38 +0700

    perubahan github

--- android/app/src/main/kotlin/com/example/pbo_meal_planner/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pbo_meal_planner
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/services/assistant_services.dart
+++
@@ -1,1 +0,0 @@
-const String GEMINI_API_KEY = "AIzaSyDw9o6y-1mK72_MXFnpsYNZKLPrR45dgh4";
